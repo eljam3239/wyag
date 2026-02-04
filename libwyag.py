3050e0e9c8d13b17b3e83e5c73b0edf20bccc407
Elijah James <elijames2301@gmail.com>
2026-02-04 11:06:19 -0500

    write path utils

--- libwyag.py
+++ libwyag.py
@@ -38,6 +38,27 @@ class GitRepository (object):
             vers = int(self.conf.get("core", "repositoryformatversion"))
             if vers != 0:
                 raise Exception("Unsupported repositoryformatversion: {vers}")
+def repo_path(repo, *path):
+    return os.path.join(repo.gitdir, *path)
+
+def repo_file(repo, *path, mkdir=False):
+    if repo_dir(repo, *path[:-1], mkdir=mkdir):
+        return repo_path(repo, *path)
+
+def repo_dir(repo, *path, mkdir=False):
+    path = repo_path(repo, *path)
+
+    if os.path.exists(path):
+        if (os.path.isdir(path)):
+            return path
+        else:
+            raise Exception(f"Not a directory {path}")
+
+    if mkdir:
+        os.makedirs(path)
+        return path
+    else:
+        return None
 
 def main(argv=sys.argv[1:]):
     args = argparser.parse_args(argv)
