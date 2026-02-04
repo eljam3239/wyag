668f1c27fd14c77f14c8f03566f4a29755074f5f
Elijah James <elijames2301@gmail.com>
2026-02-04 11:22:44 -0500

    Write repo_create

--- libwyag.py
+++ libwyag.py
@@ -38,6 +38,7 @@ class GitRepository (object):
             vers = int(self.conf.get("core", "repositoryformatversion"))
             if vers != 0:
                 raise Exception("Unsupported repositoryformatversion: {vers}")
+
 def repo_path(repo, *path):
     return os.path.join(repo.gitdir, *path)
 
@@ -59,6 +60,39 @@ def repo_dir(repo, *path, mkdir=False):
         return path
     else:
         return None
+
+def repo_create(path):
+    """Create a new repository at path."""
+
+    repo = GitRepository(path, True)
+
+    if os.path.exists(repo.worktree):
+        if not os.path.isdir(repo.worktree):
+            raise Exception (f"{path} is not a directory!")
+        if os.path.exists(repo.gitdir) and os.listdir(repo.gitdir):
+            raise Exception (f"{path} is not empty!")
+    else:
+        os.makedirs(repo.worktree)
+
+    assert repo_dir(repo, "branches", mkdir=True)
+    assert repo_dir(repo, "objects", mkdir=True)
+    assert repo_dir(repo, "refs", "tags", mkdir=True)
+    assert repo_dir(repo, "refs", "heads", mkdir=True)
+
+    # .git/description
+    with open(repo_file(repo, "description"), "w") as f:
+        f.write("Unnamed repository: edit this file 'description' to name the repository.\n")
+
+    # .git/HEAD 
+    with open(repo_file(repo, "HEAD"), "w") as f:
+        f.write("ref: refs/heads/master\n")
+
+    with open(repo_file(repo, "config"), "w") as f:
+        config = repo_default_config()
+        config.write(f)
+
+    return repo
+
 
 def main(argv=sys.argv[1:]):
     args = argparser.parse_args(argv)
