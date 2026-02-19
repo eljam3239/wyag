a969d5e2de7bb21120169d884c21d19cf7e799ab
Elijah James <elijames2301@gmail.com>
2026-02-19 10:58:47 -0500

    write object write

--- libwyag.py
+++ libwyag.py
@@ -170,6 +170,19 @@ def object_read(repo, sha)
 
         return c(raw[y+1:])
 
+def object_write(obj, repo=None):
+    data = obj.serialize()
+    result = obj.fmt + b' ' + str(len(data)).encode() + b'\x00' + data
+    sha = hashlib.sha1(result).hexdigest()
+
+    if repo:
+        path = repo_file(repo, "objects", sha[0:2], sha[2:], mkdir=True)
+
+        if not os.path.exists(path):
+            with open(path, 'wb') as f:
+                f.write(zlib.compress(result))
+    return sha
+
 def main(argv=sys.argv[1:]):
     args = argparser.parse_args(argv)
     match args.command:
